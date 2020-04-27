d995868cce807110c5742a84f1938a43e9b34e9f
Amiro <mikeamiro@digitallegion.co.ke>
2020-04-27 09:08:32 +0300

    Initial Commit

---
+++ Slider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slider
+//
+//  Created by Michael Amiro on 27/04/2020.
+//  Copyright © 2020 Michael Amiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SliderTests/SliderTests.swift
@@ -0,0 +1,34 @@
+//
+//  SliderTests.swift
+//  SliderTests
+//
+//  Created by Michael Amiro on 27/04/2020.
+//  Copyright © 2020 Michael Amiro. All rights reserved.
+//
+
+import XCTest
+@testable import Slider
+
+class SliderTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
